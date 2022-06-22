0642432fc8a2700044064502a0420c8811dcf168
Alexandr Matanov <xadx@inbox.ru>
2022-06-23 02:32:23 +0300

    trash comments are deleted

--- Lection1/DogAndFriends/Program.cs
+++ Lection1/DogAndFriends/Program.cs
@@ -4,28 +4,16 @@
 // Собака с полученной скоростью бежит от первого друга ко второму и обратно.
 // Сколько раз собака пробежит туда-сюда, прежде чем друзья встретяться.
 // Расстояние встречи принимаем за 10м
-// int age = Convert.ToInt32(Console.ReadLine());
 
-//Console.Clear();
+Console.Clear();
 Console.Write("Введите расстояние между друзьями: ");
 int? distance = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine(distance);
 Console.Write("Введите скорость первого: ");
 int? firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine(firstFriendSpeed);
 Console.Write("Введите скорость второго: ");
 int? secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine(secondFriendSpeed);
 Console.Write("Введите скорость собаки: ");
 int? dogSpeed = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine(dogSpeed);
-
-//int dist = Convert.ToInt32(distance);
-//int fstSp = Convert.ToInt32(firstFriendSpeed);
-//int sndSp = Convert.ToInt32(Console.ReadLine());
-//int dogSp = Convert.ToInt32(Console.ReadLine());
-
-//Console.WriteLine(distance+firstFriendSpeed+secondFriendSpeed+dogSpeed);
 
 int distanceLimit = 10;
 bool dogDirection = true;
