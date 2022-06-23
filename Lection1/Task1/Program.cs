51e72b17c0b37b0a3c0452ecd51a861e76e7d7a3
Alexandr Matanov <xadx@inbox.ru>
2022-06-23 23:04:22 +0300

    seminar end

---
+++ Lection1/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Выводит название дня недели по полученному из консоли номеру
+
+Console.WriteLine("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    if (number < 8)
+    {
+        if (number == 1)
+        {
+            Console.WriteLine("Понедельник");
+        }
+        if (number == 2)
+        {
+            Console.WriteLine("Вторник");
+        }
+        if (number == 3)
+        {
+            Console.WriteLine("Среда");
+        }
+        if (number == 4)
+        {
+            Console.WriteLine("Четверг");
+        }
+        if (number == 5)
+        {
+            Console.WriteLine("Пятница");
+        }
+        if (number == 6)
+        {
+            Console.WriteLine("Суббота");
+        }
+        if (number == 7)
+        {
+            Console.WriteLine("Воскресенье");
+        }
+    }
+    else { Console.WriteLine("Номер больше 7"); }
+}
+else { Console.WriteLine("Номер меньше 1"); }
