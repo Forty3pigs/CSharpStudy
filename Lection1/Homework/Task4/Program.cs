bd45ed1cf7c1ba8f38af9a26e8b93db2777dedbb
Alexandr Matanov <xadx@inbox.ru>
2022-06-24 02:21:45 +0300

    Task 4 done

---
+++ Lection1/Homework/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("All non numeric simbols will be equal to 0 ");
+Console.Write("Enter your number: ");
+int.TryParse(Console.ReadLine(), out int numberN);
+int i = 0;
+if (numberN > 0)
+{
+    while (i < numberN)
+    {
+        Console.Write($"{i} ");
+        i = i + 2;
+    }
+}
+else Console.Write("Too small");
