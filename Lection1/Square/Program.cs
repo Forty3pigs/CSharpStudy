4742dd4205089934a6c4ca36e010f1a9a0b29927
Alexandr Matanov <xadx@inbox.ru>
2022-06-23 23:04:22 +0300

    seminar end

---
+++ Lection1/Square/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //            Math.Pow(number, 2); // number в степени 2
+            //             
+            int square = number * number;
+            number *= number;
+            Console.WriteLine("Квадрат " + square);
+            Console.WriteLine($"Квадрат числа: {number}");
+        }
+    }
+}
