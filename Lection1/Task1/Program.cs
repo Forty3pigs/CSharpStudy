39f011a2f7c1b6d180f45e6ae27fa708f0708bbe
Alexandr Matanov <xadx@inbox.ru>
2022-06-23 23:40:14 +0300

    first task in hw is ready

--- Lection1/Task1/Program.cs
+++ Lection1/Task1/Program.cs
@@ -1,40 +1,46 @@
 ﻿// Выводит название дня недели по полученному из консоли номеру
 
-Console.WriteLine("Введите номер дня недели: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number > 0)
+internal class Program
 {
-    if (number < 8)
+    private static void Main(string[] args)
     {
-        if (number == 1)
+        Console.WriteLine("Введите номер дня недели: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
         {
-            Console.WriteLine("Понедельник");
+            if (number < 8)
+            {
+                if (number == 1)
+                {
+                    Console.WriteLine("Понедельник");
+                }
+                if (number == 2)
+                {
+                    Console.WriteLine("Вторник");
+                }
+                if (number == 3)
+                {
+                    Console.WriteLine("Среда");
+                }
+                if (number == 4)
+                {
+                    Console.WriteLine("Четверг");
+                }
+                if (number == 5)
+                {
+                    Console.WriteLine("Пятница");
+                }
+                if (number == 6)
+                {
+                    Console.WriteLine("Суббота");
+                }
+                if (number == 7)
+                {
+                    Console.WriteLine("Воскресенье");
+                }
+            }
+            else { Console.WriteLine("Номер больше 7"); }
         }
-        if (number == 2)
-        {
-            Console.WriteLine("Вторник");
-        }
-        if (number == 3)
-        {
-            Console.WriteLine("Среда");
-        }
-        if (number == 4)
-        {
-            Console.WriteLine("Четверг");
-        }
-        if (number == 5)
-        {
-            Console.WriteLine("Пятница");
-        }
-        if (number == 6)
-        {
-            Console.WriteLine("Суббота");
-        }
-        if (number == 7)
-        {
-            Console.WriteLine("Воскресенье");
-        }
+        else { Console.WriteLine("Номер меньше 1"); }
     }
-    else { Console.WriteLine("Номер больше 7"); }
 }
-else { Console.WriteLine("Номер меньше 1"); }
