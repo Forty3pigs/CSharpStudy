127ebf94d187fed2fdfd8db0a2fff0671cc8640f
Alexandr Matanov <xadx@inbox.ru>
2022-06-24 02:07:19 +0300

    task 3 done

---
+++ Lection1/Homework/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("All non numeric simbols will be equal to 0 ");
+Console.Write("Enter the number: ");
+int.TryParse(Console.ReadLine(), out int number);
+
+if (number % 2 == 0) { Console.Write("Y"); }
+else { Console.Write("N"); }
