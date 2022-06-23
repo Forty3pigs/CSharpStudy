aea5cbf9617c7feed0c956a983a8c79b6bcb3562
Alexandr Matanov <xadx@inbox.ru>
2022-06-23 23:40:14 +0300

    first task in hw is ready

---
+++ Lection1/Homework/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Enter first number: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first > second)
+{
+    Console.WriteLine(first + " is greater than " + second);
+}
+else
+{
+    Console.WriteLine(second + " is greater than " + first);
+}
