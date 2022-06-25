6247b9029d12d0237abcebfad047386cbc4e0c9b
Alexandr Matanov <xadx@inbox.ru>
2022-06-25 17:06:56 +0300

    gitignor fix

--- Lection1/tmpTesting/Program.cs
+++ Lection1/tmpTesting/Program.cs
@@ -1,12 +1,13 @@
-﻿while (true)
-{
-    Console.Write("Введите число: ");
-    string text = Console.ReadLine();
-    if (int.TryParse(text, out int number))
-    {
-        Console.WriteLine("Вы ввели число {0}", number);
-        break;
-    }
-    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
-}
+﻿// while (true)
+// {
+//     Console.Write("Введите число: ");
+//     string text = Console.ReadLine();
+//     if (int.TryParse(text, out int number))
+//     {
+//         Console.WriteLine("Вы ввели число {0}", number);
+//         break;
+//     }
+//     Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+// }
 //asdsad
+// asd
