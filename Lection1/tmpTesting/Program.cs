958bb39fda1fd41a900fd3709e32dfefc1f473d5
Alexandr Matanov <xadx@inbox.ru>
2022-06-25 13:56:52 +0300

    lection 2 started

--- Lection1/tmpTesting/Program.cs
+++ Lection1/tmpTesting/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Write("Введите число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        Console.WriteLine("Вы ввели число {0}", number);
+        break;
+    }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
--- Lection2/IntroMethod/Program.cs
+++ Lection2/IntroMethod/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+int a1 = 4;
+int b1 = 23;
+int c1 = 14;
+int a2 = 4;
+int b2 = 2;
+int c2 = 84;
+int a3 = 3;
+int b3 = 11;
+int c3 = 1;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
