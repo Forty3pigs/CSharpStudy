0b507d6d24813de1b8ad615d1878c12f1ccd8ae1
Alexandr Matanov <xadx@inbox.ru>
2022-06-26 15:00:44 +0300

    Add RandomCode folder w/ Abs fnd reverse value programs

---
+++ RandomCode/Absolut/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу вывода модуля числа
+try
+{
+    Console.WriteLine("Enter a number");
+    if (int.TryParse(Console.ReadLine(), out int abs))
+        Console.WriteLine($"Absolut of {abs} = {Math.Abs(abs)}");
+}
+catch
+{
+    Console.WriteLine("Huston, we have a trouble");
+}
---
+++ RandomCode/ReverseValue/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает значение, а на выходе показывает обратное значение.
+// Например:
+// 2 -> 0,5 или 0,25 -> 4
+
+try
+{
+    Console.WriteLine("Enter a number");
+    if (double.TryParse(Console.ReadLine(), out double abs) && abs != 0)
+        Console.WriteLine($"Reverse value of {abs} = {1 / abs}");
+    else Console.WriteLine("Wrong input");
+}
+catch
+{
+    Console.WriteLine("Huston, we have a trouble");
+}
