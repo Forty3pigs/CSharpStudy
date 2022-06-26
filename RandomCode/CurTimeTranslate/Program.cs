4058f1c731692009b93addc32aa507c6f050ad37
Alexandr Matanov <xadx@inbox.ru>
2022-06-26 22:17:50 +0300

    Curent Time Translate started

---
+++ RandomCode/CurTimeTranslate/Program.cs
@@ -0,0 +1,21 @@
+﻿// Нужно получить системное время и вывести его по-русски
+
+// ВозвращаемыйТипДанных ИмяМетода([ТипДанных1 ИмяАргумента1, ...])
+// {
+//   ТелоМетода;
+//   return ЗначениеСоответствующееВозвращаемомуТипуДанных;
+// }
+string GetHour(int Hour, )
+
+string localHour = DateTime.Now.ToString("HH");
+string localMinute = DateTime.Now.ToString("mm");
+Console.WriteLine(localHour);
+Console.WriteLine(localMinute);
+int.TryParse(localHour, out int Hour);
+int.TryParse(localHour, out int Minute);
+
+if (Hour >= 5)  //часов
+
+    if (Hour < 5 && Hour != 1)   //часа
+
+else //час
