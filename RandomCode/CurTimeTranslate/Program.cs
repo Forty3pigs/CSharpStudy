b68e20d0eaac117325d92c83f5e67aacec83d2ba
Alexandr Matanov <xadx@inbox.ru>
2022-06-28 10:37:26 +0300

    add comments

--- RandomCode/CurTimeTranslate/Program.cs
+++ RandomCode/CurTimeTranslate/Program.cs
@@ -8,13 +8,16 @@
 // Если без return, то вместо ВозвращаемыйТипДанных пишется void
 
 string TransTime(int Time, string HorM)
+// Метод получает на вход 2-значное значение часов или минут и уточнение часы это или минуты прилетели
 {
     string[] capitalOneToTwenty = {"Ноль", "Один", "Два", "Три", "Четыре", "Пять", "Шесть", "Семь", "Восемь", "Девять",
     "Десять", "Одинадцать", "Двенадцать", "Тринадцать", "Четырнадцать", "Пятнадцать", "Шестнадцать", "Семнадцать",
     "Восемнадцать", "Девятнадцать", "Двадцать"};
+    // тут накидываем базовые значения для часов, которые для минут будем переводить ToLower
     string[] addForMinutes = { "одна", "две", "двадцать ", "тридцать ", "сорок ", "пятьдесят " };
+    // добавочные значения, необходимые для вывода минут
 
-    string txtTime = "";
+    string txtTime = ""; // сюда прилетает сконвертированное в буквы время (часы или минуты)
 
     if (HorM == "Hour")
     {
@@ -43,17 +46,19 @@ string TransTime(int Time, string HorM)
     return txtTime;
 }
 
-string localHour = DateTime.Now.ToString("HH");
-string localMinute = DateTime.Now.ToString("mm");
+string localHour = DateTime.Now.ToString("HH");     //получаем текущее время: только часы 2-значным числом
+string localMinute = DateTime.Now.ToString("mm");   //получаем текущее время: только минуты 2-значным числом
 Console.WriteLine(localHour);
 Console.WriteLine(localMinute);
 int.TryParse(localHour, out int Hour);
 int.TryParse(localMinute, out int Minute);
-string h = "Hour";
-string m = "Minute";
-string endingH = "";
-string endingM = "";
+string h = "Hour";                                  //параметр, который улетит в метод что будем преобразовывать
+string m = "Minute";                                // то же
+string endingH = "";                                // переменная для часа/часов/час
+string endingM = "";                                // аналогичное для минут
 
+
+// ниже разбираемся с окончаниями
 if (Hour >= 5)  //часов
     endingH = " часов ";
 if (Hour < 5 && Hour != 1)   //часа
@@ -68,7 +73,7 @@ if (Minute < 5 && Minute != 1)
 if (Minute == 1)
     endingM = " минута";
 
-Console.Write(TransTime(Hour, h) + endingH + TransTime(Minute, m) + endingM);
+Console.Write(TransTime(Hour, h) + endingH + TransTime(Minute, m) + endingM); //собираем всё в кучу
 
 
 // Один, Два, Три, Четыре, Пять, Шесть, Семь, Восемь, Девять, Десять, 
