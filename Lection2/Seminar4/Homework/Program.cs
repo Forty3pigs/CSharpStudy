70c46bbad6cc0b8b74ae754015402df449231be4
Alexandr Matanov <xadx@inbox.ru>
2022-07-08 10:48:03 +0300

    add comments

--- Lection2/Seminar4/Homework/Program.cs
+++ Lection2/Seminar4/Homework/Program.cs
@@ -44,13 +44,13 @@
         {
             Console.Write("\nEnter size of array: ");
             int.TryParse(Console.ReadLine(), out int size);
-            //int size = Convert.ToInt32(Console.ReadLine());
             return size;
         }
+        // массив целых только с заданным кол-вом разрядов
         public int[] CreateArray(int size, int digitQuantity)
         {
             int[] array = new int[size];
-            int start = Convert.ToInt32(Math.Pow(10, digitQuantity - 1));
+            int start = Convert.ToInt32(Math.Pow(10, digitQuantity - 1));   // задаём разрядность массива
             int end = Convert.ToInt32(Math.Pow(10, digitQuantity));
             Console.WriteLine("\nArray generated: ");
             for (int i = 0; i < size; i++)
@@ -62,6 +62,7 @@
             Console.WriteLine("");
             return array;
         }
+        // массив целых в диапазоне от start до end-1
         public int[] CreateArray(int size)
         {
             int[] array = new int[size];
@@ -76,8 +77,9 @@
             Console.WriteLine("");
             return array;
         }
+        // массив вещественных в диапазоне от start до end-1
         public double[] CreateArray(int size, bool d)
-        // bool d - заглушка. Пожно прикрутить отрезание десятичных знаков до некоторого значения
+        // bool d - заглушка. Можно прикрутить отрезание десятичных знаков до некоторого значения
         {
             double[] array = new double[size];
             int start = -999;
