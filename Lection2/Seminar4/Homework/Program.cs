527a11780a81b0684dbf8784765f10707fafbb6e
Alexandr Matanov <xadx@inbox.ru>
2022-07-10 03:05:36 +0300

    full restructure of homework

--- Lection2/Seminar4/Homework/Program.cs
+++ Lection2/Seminar4/Homework/Program.cs
@@ -6,95 +6,35 @@
         {
             Console.Clear();
             ConsoleKeyInfo choise;
-            Console.WriteLine("Press:   1 for start task1 \n \t 2 for start task2 \n \t 3 for start task3 \nOr any other key for quit");
+            TaskText.PrintIntro();
             choise = Console.ReadKey();
-            if (choise.Key == ConsoleKey.D1)
+            LunchTask(choise);
+        }
+
+        public static void LunchTask(ConsoleKeyInfo ch)
+        {
+            if (ch.Key == ConsoleKey.D1)
             {
-                Console.WriteLine("\nЗадача 34: Задайте массив заполненный случайными положительными трёхзначными числами. ");
-                Console.WriteLine("Напишите программу, которая покажет количество чётных чисел в массиве.");
-                Console.WriteLine("\nДля продолжения нажмите любую клавишу");
-                Console.ReadKey();
-                var t1 = new Task34();
-                t1.solution();
+                TaskText.Text34();
+                TaskText.TaskWait();
+                var t1 = new Tasks();
+                t1.solution34();
             }
-            if (choise.Key == ConsoleKey.D2)
+            if (ch.Key == ConsoleKey.D2)
             {
-                Console.WriteLine("\nЗадача 36: Задайте одномерный массив, заполненный случайными числами.");
-                Console.WriteLine("Найдите сумму элементов, стоящих на нечётных позициях.");
-                Console.WriteLine("\nДля продолжения нажмите любую клавишу");
-                Console.ReadKey();
-                var t2 = new Task36();
-                t2.solution();
+                TaskText.Text36();
+                TaskText.TaskWait();
+                var t2 = new Tasks();
+                t2.solution36();
             }
-            if (choise.Key == ConsoleKey.D3)
+            if (ch.Key == ConsoleKey.D3)
             {
-                Console.WriteLine("\nЗадача 38: Задайте массив вещественных чисел.");
-                Console.WriteLine("Найдите разницу между максимальным и минимальным элементов массива.");
-                Console.WriteLine("\nДля продолжения нажмите любую клавишу");
-                Console.ReadKey();
-                var t3 = new Task38();
-                t3.solution();
+                TaskText.Text38();
+                TaskText.TaskWait();
+                var t3 = new Tasks();
+                t3.solution38();
             }
             else return;
         }
     }
-    class Arrays
-    {
-        public static int AskSize()
-        {
-            Console.Write("\nEnter size of array: ");
-            int.TryParse(Console.ReadLine(), out int size);
-            return size;
-        }
-        // массив целых только с заданным кол-вом разрядов
-        public int[] CreateArray(int size, int digitQuantity)
-        {
-            int[] array = new int[size];
-            int start = Convert.ToInt32(Math.Pow(10, digitQuantity - 1));   // задаём разрядность массива
-            int end = Convert.ToInt32(Math.Pow(10, digitQuantity));
-            Console.WriteLine("\nArray generated: ");
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = new Random().Next(start, end);
-
-                Console.Write($"{array[i]} ");
-            }
-            Console.WriteLine("");
-            return array;
-        }
-        // массив целых в диапазоне от start до end-1
-        public int[] CreateArray(int size)
-        {
-            int[] array = new int[size];
-            int start = -999;
-            int end = 1000;
-            Console.WriteLine("\nArray generated: ");
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = new Random().Next(start, end);
-                Console.Write($"{array[i]} ");
-            }
-            Console.WriteLine("");
-            return array;
-        }
-        // массив вещественных в диапазоне от start до end-1
-        public double[] CreateArray(int size, bool d)
-        // bool d - заглушка. Можно прикрутить отрезание десятичных знаков до некоторого значения
-        {
-            double[] array = new double[size];
-            int start = -999;
-            int end = 1000;
-            Random rand = new Random();
-            Console.WriteLine("\nArray generated: ");
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = rand.Next(start, end) + rand.NextDouble();
-
-                Console.Write($"\n{array[i]} ");
-            }
-            Console.WriteLine("");
-            return array;
-        }
-    }
-
 }
