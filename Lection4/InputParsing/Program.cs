25e318398881096f659dcd0a03dace299bf08466
Alexandr Matanov <xadx@inbox.ru>
2022-07-17 07:20:27 +0300

    l4 input parsing. 3rd step. create array of arrays by select + split

--- Lection4/InputParsing/Program.cs
+++ Lection4/InputParsing/Program.cs
@@ -6,9 +6,13 @@ string text = "(0,1) (2,3) (4,5) (6,7"
 
 Console.WriteLine(text);
 var data = text.Split(" ")
-                .ToArray();
+               .Select(item => item.Split(","))
+               .ToArray();
 
 for (int i = 0; i < data.Length; i++)
 {
-    Console.WriteLine(data[i]);
+    for (int j = 0; j < data[i].Length; j++)
+    {
+        Console.WriteLine(data[i][j]);
+    }
 }
