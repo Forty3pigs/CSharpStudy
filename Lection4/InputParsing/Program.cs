9859d1cd0d82afed844edd21459e8b30e1882ad2
Alexandr Matanov <xadx@inbox.ru>
2022-07-17 07:14:57 +0300

    l4 input parsing. 2nd step. replace '(' and ')'

--- Lection4/InputParsing/Program.cs
+++ Lection4/InputParsing/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
 
-string text = "(0,1) (2,3) (4,5) (6,7";
+string text = "(0,1) (2,3) (4,5) (6,7"
+              .Replace("(", "")
+              .Replace(")", "");
 
+Console.WriteLine(text);
 var data = text.Split(" ")
                 .ToArray();
 
