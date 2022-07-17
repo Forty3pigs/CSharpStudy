34521eee0f4e75dd87d1f8081b9d63c59a5297d9
Alexandr Matanov <xadx@inbox.ru>
2022-07-17 07:10:37 +0300

    l4 input parsing. 1st step. split.

---
+++ Lection4/InputParsing/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+string text = "(0,1) (2,3) (4,5) (6,7";
+
+var data = text.Split(" ")
+                .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
