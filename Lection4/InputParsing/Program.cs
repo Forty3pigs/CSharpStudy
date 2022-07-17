3c500d7fc4d650a313018904a7afc280ef8a5e33
Alexandr Matanov <xadx@inbox.ru>
2022-07-17 07:27:41 +0300

    l4 input parsing. 4th step. transform data to array of tuples

--- Lection4/InputParsing/Program.cs
+++ Lection4/InputParsing/Program.cs
@@ -7,12 +7,10 @@ string text = "(0,1) (2,3) (4,5) (6,7"
 Console.WriteLine(text);
 var data = text.Split(" ")
                .Select(item => item.Split(","))
+               .Select(arr => (int.Parse(arr[0]), int.Parse(arr[1])))
                .ToArray();
 
 for (int i = 0; i < data.Length; i++)
 {
-    for (int j = 0; j < data[i].Length; j++)
-    {
-        Console.WriteLine(data[i][j]);
-    }
+    Console.WriteLine(data[i]);
 }
