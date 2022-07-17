5e2c0523895dcda0500f425ee7070da31b61d9b0
Alexandr Matanov <xadx@inbox.ru>
2022-07-17 07:36:05 +0300

    l4 input parsing. 6th step. naming of the tuple elements

--- Lection4/InputParsing/Program.cs
+++ Lection4/InputParsing/Program.cs
@@ -7,10 +7,10 @@ string text = "(0,1) (2,3) (4,5) (6,7"
 Console.WriteLine(text);
 var data = text.Split(" ")
                .Select(item => item.Split(","))
-               .Select(arr => (int.Parse(arr[0]), int.Parse(arr[1])))
+               .Select(arr => (x: int.Parse(arr[0]), y: int.Parse(arr[1])))
                .ToArray();
 
 for (int i = 0; i < data.Length; i++)
 {
-    Console.WriteLine(data[i].Item1 * 10); // now it int32 here
+    Console.WriteLine(data[i].x * 10); // now it int32 here
 }
