3d0ed028634148f946999f2199d645041543defc
Alexandr Matanov <xadx@inbox.ru>
2022-07-19 05:26:51 +0300

    l4s1hw rolled back. task 54 work again yet

--- Lection4/Seminar1/Homework/Solution54.cs
+++ Lection4/Seminar1/Homework/Solution54.cs
@@ -9,30 +9,48 @@ namespace Geekbrains
         {
             tm.InputInvitation(_invParam);
             InputProcessing inp = new InputProcessing();
-            string? strArraySize = Console.ReadLine();
-            int[] curArraySize = inp.ConvertToInt(inp.SplitInput(strArraySize));
-            if (inp.InputValidation(strArraySize, curArraySize, tm))
+            string? size = Console.ReadLine();
+            if (!String.IsNullOrEmpty(size))
             {
-                Arrays arr = new Arrays();
-                int[,] curArray = arr.CreateArrayInt(curArraySize[0], curArraySize[1], _startArray, _endArray);
-                tm.GeneratedArray();
-                arr.PrintArray(curArray);
-                Console.WriteLine();
-                for (int i = 0; i < curArray.GetLength(0); i++)
+                int[] curArraySize = inp.ConvertToInt(inp.SplitInput(size));
+                if (curArraySize.Length == 2 && curArraySize[0] > 0 && curArraySize[1] > 0)
                 {
-                    int[] tmp = new int[curArray.GetLength(1)];
-                    for (int j = 0; j < curArray.GetLength(1); j++)
+                    Arrays arr = new Arrays();
+                    int[,] curArray = arr.CreateArrayInt(curArraySize[0], curArraySize[1], _startArray, _endArray);
+                    tm.GeneratedArray();
+                    arr.PrintArray(curArray);
+                    Console.WriteLine();
+                    //tm.TextWait();
+                    for (int i = 0; i < curArray.GetLength(0); i++)
                     {
-                        tmp[j] = curArray[i, j];
+                        int[] tmp = new int[curArray.GetLength(1)];
+                        for (int j = 0; j < curArray.GetLength(1); j++)
+                        {
+                            tmp[j] = curArray[i, j];
+                        }
+                        Array.Sort(tmp);
+                        for (int j = 0; j < curArray.GetLength(1); j++)
+                        {
+                            curArray[i, j] = tmp[j];
+                        }
                     }
-                    Array.Sort(tmp);
-                    for (int j = 0; j < curArray.GetLength(1); j++)
-                    {
-                        curArray[i, j] = tmp[j];
-                    }
+                    tm.ResultedArray();
+                    arr.PrintArray(curArray);
+                    tm.TextWait();
+                }
+                else
+                {
+                    tm.ErrorArraySize();
+                    tm.TextWait();
+                    Solution(tm);
                 }
             }
-            else Solution(tm);
+            else
+            {
+                tm.ErrorEmptyInput();
+                tm.TextWait();
+                Solution(tm);
+            }
         }
     }
 }
