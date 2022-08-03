cac9b8bab7053ec265da304aab8fc3cd5fdc07f6
Alexandr Matanov <xadx@inbox.ru>
2022-08-04 01:42:09 +0300

    HW basicly done

---
+++ FinalTask/MyArray.cs
@@ -0,0 +1,30 @@
+namespace Geekbrains
+{
+    public class MyArray
+    {
+        public string[] Preset0 { get; } = { "hello", "2", "world", ":-)" };
+        public string[] Preset1 { get; } = { "1234", "1567", "-2", "computer science" };
+        public string[] Preset2 { get; } = { "Russia", "Denmark", "Kazan" };
+
+        public string[] InputArray()
+        {
+            Console.WriteLine("\nВводите элементы построчно. Для завершения ввода, нажмите Enter при пустом поле ввода");
+            string[] a = new string[0];
+            int count = 0;
+            while (true)
+            {
+                string? input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                    return a;
+
+                else
+                {
+                    Array.Resize(ref a, count + 1);
+                    a[count] = input;
+                    count++;
+                }
+            }
+        }
+    }
+}
--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,1 +1,71 @@
-﻿// Final work
+﻿namespace Geekbrains
+{
+    static public class FHW
+    {
+        static public void Main()
+        {
+            Less4Array(Choise(false));
+        }
+        public static void Less4Array(string[] arr1)
+        {
+            string[] arr2 = new string[arr1.Length];
+            int count = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i].Length < 4)
+                {
+                    arr2[count] = arr1[i];
+                    count++;
+                }
+            }
+            Array.Resize(ref arr2, count);
+            Console.WriteLine();
+            Console.WriteLine($"В результате получился массив: [ {string.Join(", ", arr2)} ]");
+        }
+        public static string[] Choise(bool presets)
+
+        {
+            MyArray arr = new MyArray();
+            ConsoleKeyInfo pressed = new ConsoleKeyInfo();
+
+            if (!presets)
+            {
+                Console.WriteLine("Нажмите 1 для выбора стандартных массивов или 2 для введения собственного");
+                pressed = Console.ReadKey();
+
+                switch (pressed.Key)
+                {
+                    case ConsoleKey.D1:
+                        return Choise(true);
+
+                    case ConsoleKey.D2:
+                        return arr.InputArray();
+                    default:
+                        return Choise(true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nНажмите 1 для выбора массива [\"hello\", \"2\", \"world\", \":-)\"]" +
+                                    "\n\t2 для выбора массива [\"1234\", \"1567\", \"-2\", \"computer science\"]" +
+                                    "\n\t3 для выбора массива [\"Russia\", \"Denmark\", \"Kazan\"]");
+                pressed = Console.ReadKey();
+
+                switch (pressed.Key)
+                {
+                    case ConsoleKey.D1:
+                        return arr.Preset0;
+
+                    case ConsoleKey.D2:
+                        return arr.Preset1;
+
+                    case ConsoleKey.D3:
+                        return arr.Preset2;
+                    default:
+                        return arr.Preset0;
+                }
+            }
+
+        }
+    }
+}
