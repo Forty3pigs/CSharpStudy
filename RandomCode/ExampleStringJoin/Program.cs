4e678835f0cd9bef48ea19b3ad14b96a4b6ddf78
Alexandr Matanov <xadx@inbox.ru>
2022-08-06 04:25:22 +0300

    Some examples, sorts need to do some more

---
+++ RandomCode/ExampleStringJoin/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int n = 5;
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(string.Join(" ", array));
---
+++ RandomCode/NODandNOK/Program.cs
@@ -0,0 +1,30 @@
+﻿// НОД -наибольший общий делитель (алгоритм Эвклида)
+// НОК - наименьшее общее кратное
+
+int n = 140, m = 175;
+
+NOD(n, m);
+NOK(n, m);
+
+int NOD(int n, int m)
+{
+    while (n != m)
+    {
+        if (n > m)
+            n = n - m;
+        else
+            m = m - n;
+
+        Console.WriteLine(n);
+        Console.WriteLine(m);
+        Console.WriteLine();
+    }
+    return n;
+}
+int NOK(int n, int m)
+{
+    int result = n * m / NOD(n, m);
+    Console.WriteLine(result);
+    Console.WriteLine();
+    return result;
+}
